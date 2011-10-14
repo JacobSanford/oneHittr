1882609033a5d59bddcaf6d73a19f350b2b04bb0
Jake Sanford <jsanford@unb.ca>
2011-10-14 12:25:39 -0300

    Re-order language values into alphabetical for convenience.

--- language.php
+++ language.php
@@ -5,10 +5,10 @@ function getLanguageString($messageIdentifier) {
 	$messages = array(
 		'EN'=> array(
 			'FILENAME'=>'Filename',
+			'FILES_NOWRITE'=>'Files directory is not writable',
 			'FILE_AVAILABLE'=>'Your file is now available at:',
 			'SINGLE_ACCESS_WARNING'=>'It will be there ONLY for a single request, then it will dissapear. If you wish to access it multiple times, upload it again.',
 			'SUBMIT'=>'Submit',
-			'FILES_NOWRITE'=>'Files directory is not writable',
 		),
 	);
 	if (isset($messages[$languageCode][$messageIdentifier])) {
