7fb79fdfa0e1306cf7e6eac4774f45cdf9943a09
Jake Sanford <jsanford@unb.ca>
2011-10-14 12:30:01 -0300

    Spelling error.

--- language.php
+++ language.php
@@ -8,7 +8,7 @@ function getLanguageString($messageIdentifier) {
 			'FILES_NOWRITE'=>'Files directory is not writable',
 			'FILE_AVAILABLE'=>'Your file is now available at:',
 			'MAX_FILESIZE_WARNING'=>'Maximum Upload Size:',
-			'SINGLE_ACCESS_WARNING'=>'It will be there ONLY for a single request, then it will dissapear. If you wish to access it multiple times, upload it again.',
+			'SINGLE_ACCESS_WARNING'=>'It will be there ONLY for a single request, then it will disappear. If you wish to access it multiple times, upload it again.',
 			'SUBMIT'=>'Submit',
 		),
 	);
